5f41b1f8d266a8243281b63de71363e8e44ca889
johanrisch <johan.risch@gmail.com>
2017-09-06 19:40:12 +0200

    updated sample app hello world to comply with the guide

--- samples/HelloWorld/app/src/main/java/com/streamanalyze/helloworld/MainActivity.java
+++ samples/HelloWorld/app/src/main/java/com/streamanalyze/helloworld/MainActivity.java
@@ -4,27 +4,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.sa.engine.client.SaClient;
-import com.sa.engine.client.consumers.GenericDataConsumer;
+import com.sa.engine.client.consumers.GenericUiConsumer;
+
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.concurrent.Future;
 
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView label = (TextView)findViewById(R.id.label);
-        SaClient saClient = new SaClient.Builder(getApplication()).build();
-        saClient.postCall("hello", new Object[]{"Streaming world"}, new GenericDataConsumer() {
-            @Override
-            public void onData(final Object[] objects) {
-                runOnUiThread(new Runnable() {
+        final TextView cqOutput = (TextView) findViewById(R.id.cqOutput);
+        Button stopCqButton = (Button) findViewById(R.id.stopCqButton);
+
+        final SaClient saClient = new SaClient.Builder(getApplication()).build();
+        final Future<Integer> cqId = saClient.postQuery("select 'Hello Streaming world ' + extract(heartbeat(1))",
+                new GenericUiConsumer() {
                     @Override
-                    public void run() {
-                        label.setText(objects[0].toString());
+                    public void onData(final Object[] objects) {
+                        cqOutput.setText(objects[0].toString());
                     }
                 });
+
+        stopCqButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saClient.stop(cqId);
             }
         });
     }
