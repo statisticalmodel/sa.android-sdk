91964d8973deea604f731d09f39444a913dded58
Tore Risch <tore@streamanalyze.com>
2018-03-09 15:34:07 +0100

    Commented sa.android SDK example

--- samples/AndroidEdgeClient/app/src/main/java/com/example/sa/androidedgeclient/MainActivity.java
+++ samples/AndroidEdgeClient/app/src/main/java/com/example/sa/androidedgeclient/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.sa.androidedgeclient;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
+/****************************************************
+ * This code implements the sa.edge app for Android *
+ ***************************************************/
+
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -12,161 +12,116 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.sa.engine.client.SaClient;
-import com.sa.engine.client.consumers.GenericUiConsumer;
 import com.sa.engine.client.consumers.TypedUiConsumer;
-import com.sa.engine.pump.Pump;
-import com.sa.engine.pump.PumpFactory;
-import com.sa.engine.pump.PumpReader;
-import com.sa.engine.pump.impl.SensorPump;
-import com.sa.engine.sensors.DeviceSensors;
 
 import java.util.concurrent.Future;
 
-import callin.Tuple;
-
-public class MainActivity extends AppCompatActivity implements SaClient.Logger {
-
-    private static final int AUDIO_PERMISSION_REQUEST_CODE = 1337;
-    private EditText edgeNameEditText;
+public class MainActivity extends AppCompatActivity // Main class of app
+        implements SaClient.Logger {
+    // Define widget variables defined in layout res/layout/MainActivity.xml:
+    private TextView titleTextView;
+    private EditText connectionEditText;
     private Button startStopButton;
     private TextView log;
-    private SaClient saClient;
-    private Future<Integer> qId;
-    private TextView titleTextView;
+
+    // Define sa.engine interface variables:
+    private SaClient saClient;              // Interface to sa.engine
+    private Future<Integer> edgeListenerID; // Handle to the edge listener
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        /********************************
-         * Set up UI elements.
-         ********************************/
-        edgeNameEditText = (EditText) findViewById(R.id.edgeNameEditText);
+    protected void onCreate(Bundle savedInstanceState) { // Callback when main activity is created
+        super.onCreate(savedInstanceState);              // Call OnCreate in superclass first
+        setContentView(R.layout.activity_main);          // Load layout for main activity
+
+        //Retrieve widgets and bind to corresponding widget variables
+        connectionEditText = (EditText) findViewById(R.id.edgeNameEditText);
         startStopButton = (Button) findViewById(R.id.startStopButton);
         titleTextView = (TextView) findViewById(R.id.titleTextView);
         log = (TextView) findViewById(R.id.log);
 
         /********************************
-         * Init sa.engine
+         * Initialize sa.engine
          ********************************/
-        init();
-
-
-    }
-
-    private void init() {
-        /********************************
-         * To access the device sensors
-         * we must tell sa.android how
-         * to access them this is done
-         * by registering a pump  called
-         * "sensor".
-         ********************************/
-        PumpReader.registerPump("sensor", new PumpFactory() {
-            @Override
-            public Pump create(Tuple args, long... frameIds) {
-                return new SensorPump(MainActivity.this, frameIds);
-            }
-        });
-
         saClient = new SaClient.Builder(getApplicationContext())
-                .withConnectionListener(new SaClient.ConnectionListener() {
+                // Create a sa.engine interface for this app
+                .withConnectionListener(new SaClient.ConnectionListener()
+                { // The ConnectionListener is called when sa.engine has been initialized
                     @Override
                     public void onConnected(SaClient client) {
-                        /********************************
-                         * Register this MainActivity to
-                         * receive logging messages from
-                         * sa.engine.
-                         ********************************/
+                        // Register the handler of log messages from sa.engine:
                         saClient.registerLogger(MainActivity.this);
-                        /********************************
-                         * Init start/stop button after
-                         * sa.engine is initialized.
-                         ********************************/
+
+                        // Define behavior of start/stop button:
                         initButton();
                     }
-
+                    // Stop the program if initialization failed:
                     @Override
                     public void onConnectionError(String message) {
                         finish();
                     }
                 }).build();
-
     }
 
-    private void initButton(){
-        /********************************
-         * Define behaviour of stop/start
-         * button
-         ********************************/
+    private void initButton() {
+         // Define behaviour of stop/start button clicks
         startStopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (qId == null && validateInput()) {
-                    /**************************************************
-                     * If there is no active query and the content of
-                     * edgeNameEditText is valid, start edge listener.
-                     **************************************************/
-                    final String edgeName = edgeNameEditText.getText().toString();
-                    qId = saClient.postQuery("edge_listener('" + edgeName + "')",
-                            new TypedUiConsumer<String>() {
+                // Pick up the text in widget connectionEditText:
+                final String edgeSpec = connectionEditText.getText().toString();
+                if (edgeListenerID == null)
+                { // When the edge listener is inactive (button shows "Activate"):
+
+                    // Check that the edge specification is correct:
+                    if(!saClient.validateEdgeSpec(edgeSpec)) {
+                        connectionEditText.setError("Enter edge info as edgeName@serverAddr");
+                        return;
+                    }
+                    /*****************************************************************************
+                     * Start the edge listener by calling OSQL function edge_listener(edgeSpec): *
+                     ****************************************************************************/
+                    edgeListenerID = saClient.postCall("edge_listener", SaClient.argl(edgeSpec),
+                            new TypedUiConsumer<String>()
+                            { // OSQL functions return streams of data and errors as call-backs
                                 @Override
                                 public void onData(String data) {
                                     addTextToLog(data);
                                 }
 
                                 @Override
-                                public void error(String errorStr) {
+                                public void onError(String errorStr) {
                                     addTextToLog(errorStr);
                                 }
                             });
-                    edgeNameEditText.setVisibility(View.GONE);
-                    titleTextView.setText(edgeName);
+                    // Change state of layout to active mode
+                    connectionEditText.setVisibility(View.GONE);
+                    titleTextView.setText(edgeSpec);
                     startStopButton.setText("STOP");
+                } else {// Stop active edge listener:
+                    saClient.stop(edgeListenerID); // Send stop signal to the edge listener
+                    edgeListenerID = null;         // Indicate that the edge listener is inactive
 
-                } else {
-                    // Stop edge mode.
-                    saClient.stop(qId);
-                    qId = null;
+                    // Change the state of the layout to inactive mode
                     titleTextView.setText("sa.engine - edge client");
-                    edgeNameEditText.setVisibility(View.VISIBLE);
+                    connectionEditText.setVisibility(View.VISIBLE);
                     startStopButton.setText("ACTIVATE");
                 }
-
             }
         });
     }
 
     @Override
     public void log(final String line) {
-        /********************************
-         * Callback received from sa.engine
-         * when a log message is received.
-         * Add message to log.
-         ********************************/
+        // sa.engine calls this method when log message is to be emitted
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 addTextToLog(line);
             }
-        });
+        }); // Post a log message on the UI thread
     }
 
     private void addTextToLog(String text) {
         log.append(text + "\n");
-    }
-
-    private boolean validateInput() {
-        /********************************
-         * Check the the content of
-         * edgeNameEditText is correctly
-         * formatted
-         ********************************/
-        if (edgeNameEditText.getText().length() == 0
-                || !edgeNameEditText.getText().toString().matches("[^@]+@[^@]+")) {
-            edgeNameEditText.setError("Enter edge info as edgeName@serverAddr");
-            return false;
-        }
-        return true;
-    }
+    } // Convenience method
 }
